ac572ecdd3b5ad7e385c9e55ca41d31f4b53d40d
StephanieStralina <StephanieDRichter@gmail.com>
2024-11-26 09:50:55 -0600

    initial repo setup

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+/*----- constants -----*/
+const Audio1 = '';
+const Audio2 = '';
+const Audio3 = '';
+const Audio4 = '';
+
+/*----- state variables -----*/
+let rounds;
+let puppycatArr;
+let playerArr;
+
+
+/*----- cached elements  -----*/
+const buttons;
+const turnEl;
+const messageEl;
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
