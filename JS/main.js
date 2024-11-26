f97020fda45686a155880042b88cec2abcc96a6a
StephanieStralina <StephanieDRichter@gmail.com>
2024-11-26 11:21:05 -0600

    Add render function

--- JS/main.js
+++ JS/main.js
@@ -15,6 +15,7 @@ let playerArr;
 // const turnEl;
 // const messageEl;
 const startButton = document.getElementById('start-game');
+const howButton = document.getElementById('how-to-play');
 const controlButtons = document.getElementById('control-buttons');
 
 /*----- event listeners -----*/
@@ -23,9 +24,43 @@ startButton.addEventListener('click', init);
 
 
 /*----- functions -----*/
-function init(){
-    controlButtons.style.visibility = 'hidden';
-    puppycatArr = [];
+function init() {
+    controlButtons.style.display = 'none';
+    computerArr = [];
     playerArr = [];
     rounds = 0;
+    render();
+}
+
+function render() {
+    renderComputerMoves();
+    renderComputerPlay();
+    renderMessage();
+}
+
+function renderComputerMoves() {
+    //if array is empty, add 5
+    //else add 1
+}
+
+function renderComputerPlay() {
+    //Play sequence of sounds/colors for puppy cat choices
+    //Player turn
+}
+
+function renderMessage() {
+    //if player moves = puppycat moves
+        //puppycat turn
+    //if player move != puppy cat
+        //Display lose message
+        //show replay section button
+}
+
+
+
+
+
+function handleMove() {
+   //if clicked, make sound/color and add to array 
+   //for each move, check against puppycat array
 }
