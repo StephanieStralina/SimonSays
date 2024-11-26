3fe67116af13340034801601e5636e2bea923c3b
StephanieStralina <StephanieDRichter@gmail.com>
2024-11-26 16:27:36 -0600

    Added computer choice functionality

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,16 @@
 /*----- constants -----*/
-const Audio1 = new Audio('https://dl.dropbox.com/scl/fi/ua8v1pq3r6btv11do5xbl/puppycat4.MP3?rlkey=xb169dsjmye0a736j40y8ldmd&st=fqii7oyd&dl=0');
+const Audio1 = new Audio('https://dl.dropbox.com/scl/fi/zy36v0h1dp6ijns9hukhw/puppycat1.MP3?rlkey=uhf5i2u442fvkvz9mkbb97reu&st=4apa0sd1&dl=0');
 const Audio2 = new Audio('https://dl.dropbox.com/scl/fi/ftmj9zqar341tuks94bwt/puppycat2.MP3?rlkey=c5nkvf39ymd360ovodqrg1od9&st=xdq7j2rg&dl=0');
 const Audio3 = new Audio('https://dl.dropbox.com/scl/fi/hg7h0k9bbyhghtemhnn4u/puppycat3.MP3?rlkey=gvz13ta228rqmi3i2mzktck9a&st=wt3wb3x1&dl=0');
 const Audio4 = new Audio('https://dl.dropbox.com/scl/fi/ua8v1pq3r6btv11do5xbl/puppycat4.MP3?rlkey=xb169dsjmye0a736j40y8ldmd&st=4cmddnak&dl=0');
 
-const gameButtons = {
-    1: {},
-    2: {},
-    3: {},
-    4: {},
-}
+const gameButtons = [1, 2, 3, 4,];
 
 /*----- state variables -----*/
 let rounds;
 let puppycatArr;
 let playerArr;
+let turn;
 
 
 /*----- cached elements  -----*/
@@ -24,11 +20,13 @@ let playerArr;
 const startButton = document.getElementById('start-game');
 const howButton = document.getElementById('how-to-play');
 const controlButtons = document.getElementById('control-buttons');
+// const btnSpans = document.getElementsByClassName('choice-button');
 
 /*----- event listeners -----*/
 //button clicks
 startButton.addEventListener('click', init);
 document.querySelector('#board').addEventListener('click', handleMove);
+
 
 /*----- functions -----*/
 function init() {
@@ -36,27 +34,33 @@ function init() {
     computerArr = [];
     playerArr = [];
     rounds = 0;
+    turn = 'computer';
     render();
 }
 
 function render() {
+    while (computerArr.length < 4) {    
+        renderComputerMoves();
+    }
     renderComputerMoves();
     renderComputerPlay();
     renderMessage();
 }
 
 function renderComputerMoves() {
-    //if array is empty, add 5
-    //else add 1
-    if (computerArr.length === 0) {
-        console.log('computer array empty');
-    }
+    const btnIdx = (Math.floor(Math.random() * gameButtons.length+1));
+    computerArr.push(`b${btnIdx}`);
 }
 
 function renderComputerPlay() {
     //Play sequence of sounds/colors for puppy cat choices
         //for each item in array, handleMove sounds/colors
     //Player turn
+    computerArr.forEach(id => {
+        const computerClick = document.getElementById(id);
+        computerClick.click();
+    });
+    turn = 'player';
 }
 
 function renderMessage() {
@@ -64,18 +68,20 @@ function renderMessage() {
         //puppycat turn
     //if player move != puppy cat
         //Display lose message
-        //show replay section button
+        //show replay section button    
 }
 
 
 function handleMove(evt) {
    //if clicked, make sound/color and add to array 
    //for each move, check against puppycat array
-   if (evt.target.id === 'board') return;
+   let clickedBtn = evt.target.id;
+   if (clickedBtn === 'board') return;
    //handler for if game hasn't starter and/or don't show elements until game starts
-   if (rounds === undefined) return;
-   //Switch Statement
-   switch (evt.target.id) {
+   if (playerArr === undefined) return;
+   console.log(clickedBtn);
+   //Switch Statement for sound
+   switch (clickedBtn) {
     case 'b1': 
         Audio1.play();
         break;
@@ -89,4 +95,13 @@ function handleMove(evt) {
         Audio4.play();
         break;
    }
+   // push id to player array
+    if (turn === 'player'){
+    playerArr.push(clickedBtn);
+    //compare for computer array
+    }
 }
+
+// function compareArrays() {
+//
+//}
